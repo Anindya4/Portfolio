9ffb3f7738c51cfe51d5310e334113e210c26699
Anindya4 <anindyanandi.work@gmail.com>
2026-09-06 03:48:31 +0530

    vercel-fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -19,7 +19,7 @@ import {
 } from "../icons";
 
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";  //for backend
+const API_BASE_URL = import.meta.env.VITE_API_URL || "";  //for backend
 
 
 export const Contact = () => {
